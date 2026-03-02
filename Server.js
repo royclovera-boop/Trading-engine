2178f425fe785b960733d0fa561d277bf104f631
royclovera-boop <royclovera@gmail.com>
2026-03-02 21:38:02 +0700

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,1 +1,14 @@
+const express = require("express");
+const axios = require("axios");
 
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Trading Engine Aktif 🔥");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server jalan di port " + PORT);
+});
